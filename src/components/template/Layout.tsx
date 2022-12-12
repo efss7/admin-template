9656851c77521f45a0a759527fdfea5f9ca087a2
efss7 <ericfelipess07@gmail.com>
2022-12-12 17:57:56 -0300

    Componente Layout

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -2,20 +2,22 @@ import Content from "./Content"
 import SideMenu from "./SideMenu"
 import TopBar from "./TopBar"
 
-interface LayoutProps{
+interface LayoutProps {
     title: string
     subtitle: string
     children?: any
 }
 
-export default function Layout(props:LayoutProps){
-    return(
-        <div>
-            <SideMenu/>
-            <TopBar title={props.title} subtitle={props.subtitle}/>
-            <Content>
-                {props.children}
-            </Content>
+export default function Layout(props: LayoutProps) {
+    return (
+        <div className={`flex h-screen w-screen`}>
+            <SideMenu />
+            <div className={`flex flex-col w-full p-7 bg-gray-300 `}>
+                <TopBar title={props.title} subtitle={props.subtitle} />
+                <Content>
+                    {props.children}
+                </Content>
+            </div>
         </div>
     )
 }
