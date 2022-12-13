cbe02f5f73c8c8b5c3e8dc8e4fc8e5c231d9ebca
efss7 <ericfelipess07@gmail.com>
2022-12-13 18:41:24 -0300

    Autenticação com Google

--- src/data/context/AuthContext.tsx
+++ src/data/context/AuthContext.tsx
@@ -1,36 +1,43 @@
-import route from 'next/router'
-import { useRouter } from "next/router";
+import route from 'next/router';
 import { createContext, useState } from "react";
 import firebase from "../../firebase/config";
 import User from "../../model/User";
 
-interface AuthContextProps{
+interface AuthContextProps {
     user?: User
-    loginGoogle?: ()=> Promise<void>
+    loginGoogle?: () => Promise<void>
 }
 
 
 const AuthContext = createContext<AuthContextProps>({})
 
-// async function userNormalization(userFirebase: firebase.User): Promise<User> {
-//     const token = await userFirebase.getIdToken()
-//     return{
-//         uid: userFirebase.uid,
-//         name: userFirebase.displayName,
-//         email: userFirebase.email,
-//         token,
-//         provider: userFirebase.providerData[0].providerId,
-//         imageUrl: userFirebase.photoURL
-//     }
-// }
-
-export function AuthProvider(props){
-    const [user, setUser]=useState<User>(null)
-
-    async function loginGoogle(){
-        route.push('/')
+async function userNormalized(userFirebase: firebase.User): Promise<User> {
+    const token = await userFirebase.getIdToken()
+    return {
+        uid: userFirebase.uid,
+        name: userFirebase.displayName,
+        email: userFirebase.email,
+        token,
+        provider: userFirebase.providerData[0].providerId,
+        imageUrl: userFirebase.photoURL
     }
-    return(
+}
+
+export function AuthProvider(props) {
+    const [user, setUser] = useState<User>(null)
+
+    async function loginGoogle() {
+        const res = await firebase.auth().signInWithPopup(
+            new firebase.auth.GoogleAuthProvider()
+        )
+        if (res.user?.email) {
+            const user = await userNormalized(res.user)
+            setUser(user)
+            route.push('/')
+        }
+
+    }
+    return (
         <AuthContext.Provider value={{
             user,
             loginGoogle
