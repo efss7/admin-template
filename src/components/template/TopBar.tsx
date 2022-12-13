46f6eeeea8b7583deecc061c5f03db40238633df
efss7 <ericfelipess07@gmail.com>
2022-12-13 11:44:47 -0300

    Botão Alternar Tema

--- src/components/template/TopBar.tsx
+++ src/components/template/TopBar.tsx
@@ -1,3 +1,5 @@
+import useData from "../../data/hook/useData"
+import ButtonTheme from "./ButtonTheme"
 import Title from "./Title"
 
 interface TopBarProps {
@@ -6,9 +8,13 @@ interface TopBarProps {
 }
 
 export default function TopBar(props: TopBarProps) {
+    const {theme, switchTheme} = useData()
     return (
-        <div>
+        <div className={`flex`}>
             <Title title={props.title} subtitle={props.subtitle}/>
+            <div className={`flex flex-grow justify-end`} >
+            <ButtonTheme theme={theme} switchTheme={switchTheme}/>
+            </div>
         </div>
     )
 }
