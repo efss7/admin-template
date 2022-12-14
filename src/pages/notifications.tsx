418256b61c8b0f26e4ed478b5628176dfd7e7360
efss7 <ericfelipess07@gmail.com>
2022-12-14 01:43:26 -0300

    Retirando botão da página de notificações

--- src/pages/notifications.tsx
+++ src/pages/notifications.tsx
@@ -1,13 +1,10 @@
 import Layout from "../components/template/Layout";
-import useData from "../data/hook/useData";
 
 export default function Notifications() {
-    const { switchTheme } = useData()
     return (
         <Layout
             title="Notificações"
             subtitle="Aqui você irá gerenciar as suas notificações!">
-            <button onClick={switchTheme}>Alternar Tema</button>
         </Layout>
     )
 }
